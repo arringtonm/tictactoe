a2275f13ebea24b9344df94fa63a08f9daf5e75f
Adam Titus and Michael Arrington <michael@>
2017-09-27 09:29:39 -0700

    make click listener correlate to box id.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,9 @@
 
 $(document).ready(function() {
   var boxes = ["1x1", "1x2", "1x3", "2x1", "2x2", "2x3", "3x1", "3x2", "3x3"];
-  for (var i=0; i<=boxes.length; i++) {
+  for (var i=0; i<boxes.length; i++) {
     $("#" + boxes[i]).click(function(){
-      alert(boxes[i]);
+      alert(this.id);
     });
   }
-  // $("#1x1").click(function() {
-  //   alert("1x1");
-  // });
 });
